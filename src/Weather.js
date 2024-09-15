c262c937a1d985387b67e39ed6a07f3a79a6f581
Aletswalo <aletswalo19@gmail.com>
2024-09-15 21:30:35 +0200

    erased everything and stared again

--- src/Weather.js
+++ src/Weather.js
@@ -1,45 +1,17 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React from "react";
+
 import "./App.css";
 
 export default function Weather() {
-    const [city, setCity] = useState("");
-    const [loaded, setLoaded] = useState(false);
-    const [weather, setWeather] = useState({});
-
-    function displayWeather(response) {
-        setLoaded(true);
-        setWeather({
-            temperature: response.data.main.temp,
-            wind: response.data.wind.speed,
-            humidity: response.data.main.humidity,
-            decription: response.data.weather[0].description,
-        });
-    }
-
-    function handleSubmit(event) {
-        event.preventDefault();
-        let apiKey = "9cb72bec958f8fb02391985ed7b219d2";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(displayWeather);
-
-    }
-
-    function updateCity(event) {
-        setCity(event.target.value);
-    }
-
     return (
         <div className="Weather">
-            let form = (
-        <form onSubmit={handleSubmit}>
-            <input type="search" placeholder="Type a city" class="formControl" onChange={updateCity} />
-            <input type="submit" value="Search" class="searchInput" />
-        </form>
-            );
+            <form>
+                <input type="search" placeholder="Type a city" class="formControl" />
+                <input type="submit" value="Search" class="searchInput" />
+            </form>
+        </div>
+    )
+}
 
-               
-       </div>
 
-)}
 
