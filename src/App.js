6978e1e4f55a7084fbf958ddbeee81a2c568707a
Aletswalo <aletswalo19@gmail.com>
2024-09-15 22:58:29 +0200

    added components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 
-import './App.css';
+import React from "react";
+import Weather from "./Weather";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-  
-      
+      <div className="container">
+        <Weather defaultCity="San Francisco" />
+      </div>
     </div>
-  
   );
 }
-
-export default App;
